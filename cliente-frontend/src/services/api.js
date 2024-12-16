c2dba091f9d7603242922d1824da71f523b99847
Carlos Fernandes <69088161+carlosfernandescrypt@users.noreply.github.com>
2024-12-16 19:20:41 -0300

    Update api.js

--- cliente-frontend/src/services/api.js
+++ cliente-frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const authHeader = localStorage.getItem("auth"); // Obtém as credenciais salvas
+const authHeader = localStorage.getItem("auth"); 
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // URL do back-end
+  baseURL: "http://localhost:8080",
   headers: {
     Authorization: `Basic ${authHeader}`,
   },
